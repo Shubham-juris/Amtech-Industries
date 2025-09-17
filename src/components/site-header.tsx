cf50df9e4ef118dd1237a78f8a2a9c9313e4c7c6
sumit thakur <sumit.jurisnexa03@gmail.com>
2025-09-17 04:27:44 +0000

    now add this

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -17,9 +17,9 @@ import { cn } from '@/lib/utils';
 const menuItems = {
   'Accessories': ['Bags', 'Cap', 'Scarf', 'Sleepwear', 'Socks', 'Towels'],
   'Custom Clothing': ['White Label Clothing', 'Sublimation'],
-  'Women': ['Lingerie', 'Shrug', 'Women Coats', 'Women Dress', 'Women FullCoverUps', 'Women Jackets', 'Women Shirts', 'Women Sweater', 'Women Tops', 'Womens Beachwear', 'Womens Bottoms', 'Womens Leggings'],
+  'Women': ['Women Dress', 'Women Coats', 'Women Jackets', 'Women Top Wear', 'Women Leggings', 'Women Lingerie'],
   'Kids': ['Girls Dresses Collection', 'Infants-0-24-Months', 'Little-4-7-Yrs', 'Toddlers-2-4-Yrs'],
-  'Men': ['Men Bottoms', 'Men Hoody', 'Men Jackets', 'Men Tees', 'Mens Beachwear', 'Mens Shirts', 'Mens Underwear', 'Polo T-Shirts Manufacturers'],
+  'Men': ['Men Hoody', 'Men Jackets', 'Men Pants', 'Men T-shirts', 'Men Shirts', 'Men Underwear'],
   'Footwear': ['Flipflops', 'Formal Shoes', 'Lifestyle', 'Running Shoes'],
   'Collection': ['New Catalog', 'New Arrivals', 'Jumpsuits', 'Polo T shirts', 'Womens Beachwear', 'Mens Beachwear'],
   'Running Fitness': ['Activewear', 'Compression', 'Men Running Fitness', 'Women Running Fitness'],

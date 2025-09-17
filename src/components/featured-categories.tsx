f2e3cf171b828b2f083f0513cbe821fa0256a049
sumit thakur <sumit.jurisnexa03@gmail.com>
2025-09-17 12:51:16 +0000

    FEATURED CATEGORIES Discover the most trending products in Amtech. this

--- src/components/featured-categories.tsx
+++ src/components/featured-categories.tsx
@@ -50,12 +50,12 @@ export function FeaturedCategories() {
 
   return (
     <section 
-      className="relative grid grid-cols-1 lg:grid-cols-4 bg-[#FFFAE6] text-black" 
+      className="relative grid grid-cols-1 lg:grid-cols-3 bg-[#FFFAE6] text-black" 
       onMouseEnter={() => setIsHovered(true)} 
       onMouseLeave={() => setIsHovered(false)}
     >
         <Wave />
-      <div className="lg:col-span-3 py-20 overflow-hidden">
+      <div className="lg:col-span-2 py-20 overflow-hidden">
         <div 
           className="flex transition-transform duration-500 ease-in-out"
           style={{ transform: `translateX(-${currentIndex * (100 / ITEMS_TO_SHOW)}%)` }}
